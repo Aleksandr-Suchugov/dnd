c31e7b8577bef4cbfcb0d7f5b655fb8562bab1a6
aleksandr-suchugov <alex.coup@yandex.ru>
2022-09-11 00:49:10 +0300

    small fixes

--- src/js/Trello.js
+++ src/js/Trello.js
@@ -3,12 +3,17 @@ import { read, save } from './localStorage';
 /* eslint-disable class-methods-use-this */
 
 export default class Trello {
+  constructor() {
+    this.originList = null;
+    this.originPrevElSibling = null;
+    this.pageEvents = document.querySelector('.container');
+  }
+
   init() {
     const state = read();
     if (state) {
       document.body.innerHTML = state;
     }
-    this.pageEvents = document.querySelector('.container');
     this.pageEvents.addEventListener('click', (e) => this.onAddTask(e));
     this.pageEvents.addEventListener('mouseover', (e) => this.onMouseOver(e));
     this.pageEvents.addEventListener('mouseout', (e) => this.onMouseOut(e));
@@ -28,20 +33,21 @@ export default class Trello {
       const selectedCard = e.target.closest('.card');
       e.target.remove();
       selectedCard.insertAdjacentHTML('beforeEnd', this.newTaskForm());
-      const taskField = selectedCard.querySelector('.new-task');
+      const taskForm = selectedCard.querySelector('.new-task');
       const list = selectedCard.querySelector('.list');
-      const addBtn = taskField.querySelector('.new-task__add');
-      const cancelBtn = taskField.querySelector('.new-task__cancel');
-      const taskContent = taskField.querySelector('.new-task__content').value;
+      const addBtn = taskForm.querySelector('.new-task__add');
+      const cancelBtn = taskForm.querySelector('.new-task__cancel');
+      const textBox = taskForm.querySelector('.new-task__content');
+      textBox.removeAttribute('readonly');
       addBtn.addEventListener('click', () => {
-        taskField.remove();
-        list.insertAdjacentHTML('beforeEnd', this.newItem(taskContent));
+        taskForm.remove();
+        list.insertAdjacentHTML('beforeEnd', this.newItem(textBox.value));
         list.insertAdjacentHTML('afterEnd', this.addButton());
         save();
       });
 
       cancelBtn.addEventListener('click', () => {
-        taskField.remove();
+        taskForm.remove();
         list.insertAdjacentHTML('afterEnd', this.addButton());
       });
     }
@@ -97,6 +103,8 @@ export default class Trello {
     }
     e.preventDefault();
     this.currentItem = e.target.closest('.item');
+    this.originList = this.currentItem.parentElement;
+    this.originPrevElSibling = this.currentItem.previousElementSibling;
     if (this.currentItem) {
       this.currentItem.classList.add('dragged');
       this.pageEvents.addEventListener('mousemove', this.drag);
@@ -120,30 +128,32 @@ export default class Trello {
 
   drag(e) {
     this.currentItem = document.querySelector('.dragged');
-    if (this.currentItem === null) return false;
+    if (!this.currentItem) return;
     this.currentItem.style.position = 'absolute';
     this.currentItem.style.left = `${e.clientX - 20}px`;
     this.currentItem.style.top = `${e.clientY - 20}px`;
   }
 
   dragEnd() {
-    if (document.querySelector('.dragged') === null) return;
-    document.querySelector('.dragged').classList.remove('dragged');
+    const draggedEl = document.querySelector('.dragged');
+    if (!draggedEl) return;
+    draggedEl.classList.remove('dragged');
     this.pageEvents.removeEventListener('mousemove', this.drag);
     this.pageEvents.removeEventListener('mouseup', this.dragEnd);
     save();
   }
 
   itemDragEnd(e) {
-    const draggedItem = document.querySelector('.dragged');
-    if (draggedItem === null) return;
+    const draggedEl = document.querySelector('.dragged');
+    if (!draggedEl) return;
     const list = this.querySelector('.list');
-    if (list.querySelector('.item') === null) {
-      list.append(draggedItem);
+    if (list) {
+      list.insertBefore(draggedEl, e.target.closest('.item'));
     } else {
-      list.insertBefore(draggedItem, e.target.closest('.item'));
+      this.originList.insertBefore(draggedEl, this.originPrevElSibling);
     }
-    draggedItem.style = '';
+    draggedEl.classList.remove('dragged');
+    draggedEl.style = '';
     save();
   }
 }
